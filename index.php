6334d34ecbabee5e76249ccace7093a7827d7f53
warebey17 <warebey17@gmail.com>
2016-01-19 20:36:28 -0600

    Added structure folder. Commit to master

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-hello world
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Starter Template for Bootstrap</title>
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+
+
+
+
+  <body>
+
+
+    <div class="container">
+    	<h1> Hello World</h1>
+    </div><!-- /.container -->
+
+
+
+  </body>
+
+</html>
